6c22e1f0413e44e73b604d61279cc544f3b0a184
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-09-12 22:04:21 -0500

    add response body to tests

--- test/domains_test.exs
+++ test/domains_test.exs
@@ -16,6 +16,8 @@ defmodule DomainsTest do
       {:ok, result, response} = Simplex.Domains.create("test_domain")
       assert response.status_code == 200
       assert result == nil
+      assert response.body == %{meta: %{box_usage: '0.0055590278',
+                                        request_id: '70e2100f-fd4e-92b5-5f7d-769198125bf7'}}
     end
   end
 
@@ -24,6 +26,10 @@ defmodule DomainsTest do
       {:ok, result, response} = Simplex.Domains.list
       assert response.status_code == 200
       assert result == ["test", "test_domain"]
+      assert response.body == %{meta: %{box_usage: '0.0000071759',
+                                        request_id: '8d85ba40-c3f1-5ff5-50ea-9dfdc2fa103c'},
+                                next_token: nil,
+                                result: ['test', 'test_domain']}
     end
   end
 
@@ -32,6 +38,8 @@ defmodule DomainsTest do
       {:ok, result, response} = Simplex.Domains.delete("test_domain")
       assert response.status_code == 200
       assert result == nil
+      assert response.body == %{meta: %{box_usage: '0.0055590278',
+                                        request_id: 'e4aead9f-b877-c4dc-98eb-3cc9538699d9'}}
     end
   end
 
