c721538ab00c8d490785c1b6380782eb37a075b8
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-09-23 15:17:33 -0500

    make version configurable

--- lib/simplex/request.ex
+++ lib/simplex/request.ex
@@ -5,19 +5,19 @@ defmodule Simplex.Request do
 
   def get(params, simplex) do
     response = Simplex.simpledb_url(simplex)
-               |> signed(params, Simplex.aws_credentials(simplex))
+               |> signed(params, Simplex.configuration(simplex))
                |> HTTPoison.get
     Response.handle(params["Action"], response)
   end
 
-  def signed(url, params, aws_credentials) do
+  def signed(url, params, config) do
     uri = URI.parse(url)
 
-    query = query_string(params, aws_credentials)
+    query = query_string(params, config)
 
     request = Enum.join(["GET", uri.host, uri.path || "/", query], "\n")
 
-    signature = :crypto.hmac(:sha256, String.to_char_list(aws_credentials[:aws_secret_access_key]), String.to_char_list(request))
+    signature = :crypto.hmac(:sha256, String.to_char_list(config[:aws_secret_access_key]), String.to_char_list(request))
                 |> :base64.encode
                 |> URI.encode
                 |> String.replace("/", "%2F")
@@ -27,20 +27,20 @@ defmodule Simplex.Request do
     "#{uri.scheme}://#{uri.authority}#{uri.path || "/"}?#{query}&Signature=#{signature}"
   end
 
-  defp auth_params(aws_credentials) do
+  defp auth_params(config) do
     [
-      AWSAccessKeyId: aws_credentials[:aws_access_key],
+      AWSAccessKeyId: config[:aws_access_key],
       SignatureVersion: 2,
       SignatureMethod: "HmacSHA256",
       Timestamp: DateFormat.format!(Date.now, "{ISOz}")
-    ] ++ auth_token(aws_credentials)
+    ] ++ auth_token(config)
   end
 
   defp auth_token(%{token: token}),  do: [SecurityToken: token]
   defp auth_token(_aws_credentials), do: []
 
-  defp query_string(params, aws_credentials) do
-    Parameters.from_map(params) ++ [{:Version, "2009-04-15"}] ++ auth_params(aws_credentials)
+  defp query_string(params, config) do
+    Parameters.from_map(params) ++ [{:Version, config[:simpledb_version]}] ++ auth_params(config)
     |> Enum.sort
     |> URI.encode_query
     |> String.replace("+", "%20")
